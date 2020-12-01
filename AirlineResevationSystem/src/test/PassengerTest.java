83ebbc0777617c60d641a9dac65133657144f2be
Anuj Aryal <aryal.anuj7@gmail.com>
2020-12-01 17:47:16 -0600

    added first test in Passenger

--- AirlineResevationSystem/src/test/PassengerTest.java
+++
@@ -1,7 +0,0 @@
-package test;
-
-import org.junit.jupiter.api.Assertions;
-
-public class PassengerTest {
-	
-}
---
+++ AirlineResevationSystem/src/tests/PassengerTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import models.Passenger;
+
+public class PassengerTest {
+	private Passenger passenger;
+	
+	@BeforeAll
+	public void setUp() {
+		this.passenger = new Passenger("065", "John", "Doe", LocalDate.of(1990, 10, 10), "john@doe.com");
+	}
+	
+	@Test
+	public void testGetId() {
+		var output = passenger.getId();
+		Assertions.assertNotNull(output);
+		Assertions.assertFalse(output.isEmpty());
+		Assertions.assertEquals("065", output);
+	}
+}
