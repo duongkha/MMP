a1453a4034e5167254fe383a513e18c0eaa75f55
Kha Nguyen <dknguyen@miu.edu>
2020-12-01 11:09:32 -0600

    implementing more

--- AirlineResevationSystem/src/Airline.java
+++ AirlineResevationSystem/src/Airline.java
@@ -1,4 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Airline {
-
+	String airlineId;
+	String code;
+	String name;
+	String history;
+	List<Flight> flights;
+	
+	public Airline(String airlineId, String code, String name, String history) {
+		super();
+		this.airlineId = airlineId;
+		this.code = code;
+		this.name = name;
+		this.history = history;
+		this.flights = new ArrayList<Flight>();
+	}
+	public String getAirlineId() {
+		return airlineId;
+	}
+	public String getCode() {
+		return code;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getHistory() {
+		return history;
+	}
+	public List<Flight> getFlights() {
+		return flights;
+	}
+	public void addFlight(Flight flight) {
+		this.flights.add(flight);
+	}
 }
--- AirlineResevationSystem/src/Airport.java
+++ AirlineResevationSystem/src/Airport.java
@@ -1,4 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Airport {
-
+	String airportId;
+	String code;
+	String name;
+	List<Flight> flights;
+	Address address;
+	
+	public Airport(String airportId, String code, String name,Address address) {
+		super();
+		this.airportId = airportId;
+		this.code = code;
+		this.name = name;
+		this.address = address;
+		flights = new ArrayList<Flight>();
+	}
+	public String getAirportId() {
+		return airportId;
+	}
+	public String getCode() {
+		return code;
+	}
+	public String getName() {
+		return name;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public List<Flight> getFlights(){
+		return flights;
+	}
+	
+	public void addFlight(Flight flight) {
+		this.flights.add(flight);
+	}
 }
