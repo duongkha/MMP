02dd53023947b8ba0116e0fd79dbbd1e10cca33a
Anuj Aryal <aryal.anuj7@gmail.com>
2020-12-01 18:30:36 -0600

    added final test for Passenger

--- AirlineResevationSystem/src/tests/PassengerTest.java
+++ AirlineResevationSystem/src/tests/PassengerTest.java
@@ -22,4 +22,42 @@ public class PassengerTest {
 		Assertions.assertFalse(output.isEmpty());
 		Assertions.assertEquals("065", output);
 	}
+	
+	@Test
+	public void testGetFirstName() {
+		var output = passenger.getFirstName();
+		Assertions.assertNotNull(output);
+		Assertions.assertFalse(output.isEmpty());
+		Assertions.assertEquals("John", output);
+	}
+	
+	@Test
+	public void testGetLastName() {
+		var output = passenger.getLastName();
+		Assertions.assertNotNull(output);
+		Assertions.assertFalse(output.isEmpty());
+		Assertions.assertEquals("Doe", output);
+	}
+	
+	@Test
+	public void testGetDOB() {
+		var output = passenger.getDOB();
+		Assertions.assertNotNull(output);
+		Assertions.assertFalse(output.toString().isEmpty());
+		Assertions.assertEquals(LocalDate.of(1990, 10, 10), output);
+	}
+	
+	@Test
+	public void testGetEmailAddress() {
+		var output = passenger.getEmailAddress();
+		Assertions.assertNotNull(output);
+		Assertions.assertFalse(output.isEmpty());
+		Assertions.assertEquals("john@doe.com", output);
+	}
+	
+	@Test
+	public void testGetReservations() {
+		var output = passenger.getReservations();
+		Assertions.assertNotNull(output);
+	}
 }
